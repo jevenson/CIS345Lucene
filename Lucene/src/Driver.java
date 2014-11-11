99d773185749a8ed0e423152c4f55d05136bf027
jdmeer <jdmeer@gmail.com>
2014-11-10 18:45:49 -0600

    fix XML file extentions

--- Lucene/src/Driver.java
+++ Lucene/src/Driver.java
@@ -6,12 +6,9 @@ import org.apache.lucene.queryparser.classic.ParseException;
 public class Driver {
 
 	public static void main(String[] args) throws ParseException, IOException {
-		// TODO Auto-generated method stub
 		
-		String[] query = {"Book","lucene"};
+		LuceneXMLProcessor.GO("Book","lucene");
 		
-		
-		LuceneXMLProcessor.GO(query);
 	}
 
 }
